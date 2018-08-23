4f55a5aa1b0c2a68ad118a27d88a76b0659809ca
flo <flo.lep2607@gmail.com>
2018-08-23 16:09:42 +0200

    Update scimdb69.py

--- scimdb69.py
+++ scimdb69.py
@@ -1,15 +1,15 @@
 from imdb import IMDb
 
-['airing', 'akas', 'alternate versions', 'awards', 'connections', 'crazy credits', 'critic reviews', 'episodes', 'external reviews', 'external sites', 'faqs', 'full credits', 'goofs', 'keywords', 'locations', 'main', 'misc sites', 'news', 'official sites', 'parents guide', 'photo sites', 'plot', 'quotes', 'release dates', 'release info', 'reviews', 'sound clips', 'soundtrack', 'synopsis', 'taglines', 'technical', 'trivia', 'tv schedule', 'video clips', 'vote details']
+#['airing', 'akas', 'alternate versions', 'awards', 'connections', 'crazy credits', 'critic reviews', 'episodes', 'external reviews', 'external sites', 'faqs', 'full credits', 'goofs', 'keywords', 'locations', 'main', 'misc sites', 'news', 'official sites', 'parents guide', 'photo sites', 'plot', 'quotes', 'release dates', 'release info', 'reviews', 'sound clips', 'soundtrack', 'synopsis', 'taglines', 'technical', 'trivia', 'tv schedule', 'video clips', 'vote details']
 
 
 # create an instance of the IMDb class
 ia = IMDb()
 fich=open("moviestitles.txt","w")
-# get a movie
+
 a,b,c,d,e,f,g=0,0,0,0,0,0,0
 
-while(str(a)+str(b)+str(c)str(d)+str(e)+str(f)+str(g))=!'7131622':
+while(str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g))!='7131622': 
 	movie = ia.get_movie(str(a)+str(b)+str(c)str(d)+str(e)+str(f)+str(g))
 	#print('title')
 	fichier.write(str(a)+str(b)+str(c)str(d)+str(e)+str(f)+str(g))
@@ -20,6 +20,25 @@ while(str(a)+str(b)+str(c)str(d)+str(e)+str(f)+str(g))=!'7131622':
 	if g==9:
 		g=0
 		f+=1
+	else:
+		g+=1
+	if f==9:
+		f=0
+		e+=1
+	if e==9:
+		e=0
+		d+=1
+	if d==9:
+		d=0
+		c+=1
+	if c==9:
+		c=0
+		b+=1
+	if b==9:
+		b=0
+		a+=1
+	print(str(a)+str(b)+str(c)+str(d)+str(e)+str(f)+str(g))
+	
 
 
 #print('/nplo/n')
